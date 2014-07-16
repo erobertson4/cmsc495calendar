851cc37d89c8dc168d2a2799a5c40203a00d5d75
mjacobs23 <matt@whereitzat.com>
2014-07-16 11:08:59 -0400

    Merge origin/master
    Conflicts:
    	src/model/bean/UserBean.java
    	src/presenter/LoginPresenter.java
    	src/view/ui/LoginScreenView.java

--- src/dbConnection/DBConnect.java
+++ src/dbConnection/DBConnect.java
@@ -20,8 +20,6 @@ public class DBConnect {
     private static String url = null;
     private static Properties info = null;
     private static Connection conn = null;
-//    private Statement stmt = null; 
-//    ResultSet rset = null;
     
     // method to connect to database
     public static Connection connect() throws SQLException {
@@ -43,31 +41,6 @@ public class DBConnect {
             info.put("user", "cs495b04");
             info.put("password", "h4m5t4n9");
             conn = DriverManager.getConnection(url, info);
-//            stmt = conn.createStatement();
-//            rset = stmt.executeQuery("SELECT * FROM USERS_T");
-//            
-//            //extract data from result set
-//            while(rset.next()) {
-//                int dbUserID = rset.getInt("USERID");
-//                String dbFirstName = rset.getString("FIRSTNAME");
-//                String dbLastName = rset.getString("LASTNAME");
-//                String dbUserName = rset.getString("USERNAME");
-//                String dbPassword = rset.getString("PASSWORD");
-//                
-//                // display data values
-//                System.out.println(dbUserID);
-//                System.out.println(dbFirstName);
-//                System.out.println(dbLastName);
-//                System.out.println(dbUserName);
-//                System.out.println(dbPassword);
-//                
-//                
-//            }
-//            
-//            // close all db connections
-//            rset.close();
-//            stmt.close();
-//            conn.close();
 
         } // end try
         
@@ -76,30 +49,6 @@ public class DBConnect {
             Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, "Error connecting to database", ex);
         } // end catch: SQLException
         
-        
-        // fail-all close database resources
-//        finally {    
-//            try {
-//                if(rset != null) rset.close();
-//            }
-//            catch(SQLException ex) {
-//                Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
-//            } // end catch
-            
-//            try {
-//                if(conn!=null) conn.close();
-//            }
-//            catch(SQLException ex) {
-//                Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
-//            } // end catch
-            
-//            try {
-//                if(stmt != null) stmt.close();
-//            }
-//            catch(SQLException ex) {
-//                Logger.getLogger(DBConnect.class.getName()).log(Level.SEVERE, null, ex);
-//            } // end catch
-//        } // end finally
         
         return conn;
     } // end method: connect()
